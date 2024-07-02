8c2de94f8cd2d87e41505d21ba5a68505bf87f4b
Jaewook Kim <jaewookkim@kisti.re.kr>
2024-07-02 20:04:22 +0900

    Cython wrapper of Comm<DEVICETYPE::MKL> is implemented

--- python/test.py
+++ python/test.py
@@ -1,8 +1,8 @@
 #import pycomm.pyx
-import PySparseTensor
+from PySparseTensor import mkl_comm
 
 print("Comm test")
 print("Create comm")
-new_comm = PySparseTensor.create_mkl_comm()
-print("Copy new_comm into new2comm")
-new2_comm = PySparseTensor.copy_mkl_comm(new_comm)
+new_comm = mkl_comm()
+print("Create Comm using create_comm")
+new_2comm = new_comm.clone()
